69376878f14a630ab3dae1bece8b6be557858e0c
Andrew Blinnikov <blnnikoff@gmail.com>
2023-01-08 23:09:29 +0300

    refactor playbook

--- src/_includes/partials/playlist-paging/index.js
+++ src/_includes/partials/playlist-paging/index.js
@@ -2,15 +2,23 @@ import { createApp } from "https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es
 function Paging(props) {
     return {
         current: window.location.pathname.replace(/\/+$/g, ""),
-        list: window.atob(sessionStorage.getItem("list")).split(";"),
-        index() { return this.list.indexOf(this.current); },
-        isFirst() { return this.index() === 0; },
-        isLast() { return this.index() === this.list.length - 1; },
-        prev() { return this.list[this.index() - 1]; },
-        next() { return this.list[this.index() + 1]; },
-        format(link) {
-            const match = link.split("/");
-            return match[match.length - 1];
+        list: JSON.parse(sessionStorage.getItem("list")),
+        get index() { return this.list.map(el => el.href).indexOf(this.current); },
+        get isFirst() { return this.index === 0; },
+        get isLast() { return this.index === this.list.length - 1; },
+        get prev() {
+            if (this.isFirst) {
+                return null;
+            } else {
+                return this.list[this.index - 1].href;
+            }
+        },
+        get next() {
+            if (this.isLast) {
+                return null;
+            } else {
+                return this.list[this.index + 1].href;
+            }
         },
         goto(link) {
             if (link) {
--- src/playbook/index.js
+++ src/playbook/index.js
@@ -1,23 +1,57 @@
 import { createApp } from "https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es.js";
 function Playbook(props) {
     return {
+        editing: -1,
+        switchState: false,
         newSong: "",
+        editedSong: "",
         list: [],
+        get length() {
+            return this.list.length;
+        },
         add() {
-            if (this.newSong && !isNaN(this.newSong)) {
-                this.list.push(this.newSong);
+            const val = this.newSong;
+            if (val && !isNaN(val)) {
+                this.list.push({ label: val, href: `/hymns/${val}` });
                 this.newSong = "";
             }
-            this.$refs.songInput.focus();
+            // this.$refs.songInput.focus();
+        },
+        startEdit(index) {
+            this.editing = index;
+            this.editedSong = this.list[index].label;
+        },
+        edit(index) {
+            const val = this.editedSong;
+            if (val && !isNaN(val)) {
+                this.list[index] = { label: val, href: `/hymns/${val}` };
+                this.undo();
+            }
+        },
+        undo() {
+            this.editing = -1;
+            this.editedSong = "";
         },
         remove(index) {
             this.list.splice(index, 1);
+            this.undo();
         },
         start() {
-            const hymns = this.list.map(el => `/hymns/${el}`);
-            sessionStorage.setItem("list", window.btoa(hymns.join(";")));
-            window.location.replace(hymns[0]);
-        }
-    }
+            sessionStorage.setItem("list", JSON.stringify(this.list));
+            window.location.replace(this.list[0].href);
+        },
+        toggle() {
+            this.switchState = !this.switchState;
+            if (this.switchState) {
+                // 0 Hymn
+                // 1 Penitence
+                // 
+                // TODO add empty spaces for hymns
+                this.list.splice(1, 0, { label: "Отпущение грехов", href: "/liturgy/penitence" });
+            } else {
+                this.list = this.list.filter(el => el.href.startsWith("/hymn"));
+            }
+        },
+    };
 }
 createApp({ Playbook }).mount("#playbook-app");
